b7ff020d74b07e3832569ad210a51dea172ed065
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2024-01-22 21:57:15 -0500

    [+] More fields to Invitation class

--- src/lib.d.ts
+++ src/lib.d.ts
@@ -3,13 +3,28 @@ interface Invitation {
     title: string;
     description: string;
     location: string;
-    creator: string;
+    organizer: string;
+    participant: string;
 
     // Requirement for the meeting time
     regularity: 'once' | 'daily' | 'weekly';
     duration: number; // in minutes
     timezone: string;
     daysRequired?: number;
+
+    // Creator's availability
+    availability: Preference[][]; // Every 15 minutes
+    timeRange?: {
+        from: Date;
+        to: Date;
+    }; // Only relevant for regularity = 'once'
+}
+
+export enum Preference {
+    high = 3,
+    medium = 2,
+    low = 1,
+    none = 0,
 }
 
 declare function createLink(invitation: Invitation);
