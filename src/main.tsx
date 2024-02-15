a73fe86d86aeb8469946b7f9fdd24610516095f0
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2024-02-14 23:23:45 -0500

    [M] Rename stuff

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.scss'
+import "./global.scss"
+import 'modern-normalize/modern-normalize.css';
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
 const router = createBrowserRouter([
