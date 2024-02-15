d1d0c67b6ebfb33e4c78e470639a8215d4f16528
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2024-02-14 23:23:55 -0500

    [+] Class utility

---
+++ src/lib/ui.ts
@@ -0,0 +1,13 @@
+
+/**
+ * Convert object keys to classes depending on their values
+ *
+ * @param obj Dictionary object {classname: truthy}
+ * @param others Other classes to add
+ */
+export function clz(obj: any, others?: string)
+{
+  let cls = Object.keys(obj).filter(key => obj[key]).join(' ')
+  if (others) cls += ' ' + others
+  return cls
+}
