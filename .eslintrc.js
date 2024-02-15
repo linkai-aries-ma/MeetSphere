98b404b580c818ea6422674aed92783cc336073d
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2024-02-15 00:27:48 -0500

    [+] prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
 
         'plugin:@typescript-eslint/recommended',
         'plugin:react-hooks/recommended',
+        "prettier",
     ],
     ignorePatterns: ['dist', '.eslintrc.cjs'],
     parser: '@typescript-eslint/parser',
