a7a75414729f250f922e8508e299d943ba74437f
willjarvis-cross <72054183+WillJarvis-Cross@users.noreply.github.com>
2024-03-09 22:25:53 -0500

    remove unnecessary lines

--- backend/meetsphere/views.py
+++ backend/meetsphere/views.py
@@ -1,6 +1,6 @@
-from django.contrib.auth import authenticate, logout
+from django.contrib.auth import logout
 from rest_framework import status
-from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth.models import AnonymousUser
