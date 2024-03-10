57eca68d11f734dfedc483ab4cfbd47842367603
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2024-03-10 12:17:59 -0400

    [+] Jest unit tests

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+export default {
+  // transform: {},
+  // extensionsToTreatAsEsm: ['.ts'],
+  // other Jest configurations...
+  testEnvironment: 'jsdom',
+  resetMocks: false,
+  setupFiles: ['jest-localstorage-mock']
+}
